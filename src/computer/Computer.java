54dc2c0b8d74ed24fa2919f6fb136b8c03a64842
jyothi0911 <sivanagajyothi522@gmail.com>
2020-07-18 15:51:24 +0530

    6th task

---
+++ src/computer/Computer.java
@@ -0,0 +1,10 @@
+package computer;
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("computer model is Lenovo8");
+		
+	}
+
+
+}
---
+++ src/computer/Desktop.java
@@ -0,0 +1,16 @@
+package computer;
+
+public class Desktop  extends Computer{
+	public void desktopSize() {
+		System.out.println("dektop size is 869z");
+
+
+	}
+	public static void main(String[] args) {
+		Desktop dt= new Desktop();
+		dt.computerModel();
+		dt.desktopSize();
+	}
+	
+
+}
